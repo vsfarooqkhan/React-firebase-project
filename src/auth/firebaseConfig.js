e7a549937a14c96d3743de7bbed8ee0c57f83b66
Farooq khan <vsfarooqkhan@gmail.com>
2019-09-25 15:12:10 +0530

    Update firebaseConfig.js

--- src/auth/firebaseConfig.js
+++ src/auth/firebaseConfig.js
@@ -1,11 +1,5 @@
 import firebase from 'firebase'
 const firebaseConfig  = firebase.initializeApp({
-    apiKey: "AIzaSyCcbaL8c7eR-ZkWDrVEYSh8fOgyj96L494",
-    authDomain: "react-project-5f2cc.firebaseapp.com",
-    databaseURL: "https://react-project-5f2cc.firebaseio.com",
-    projectId: "react-project-5f2cc",
-    storageBucket: "",
-    messagingSenderId: "725737792559",
-    appId: "1:725737792559:web:fa2a05816c3861f2a712da"
+*****************************
   });
   export default firebaseConfig
